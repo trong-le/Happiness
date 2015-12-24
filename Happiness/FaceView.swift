25a97ba3408f942d10b7bc931fa87b0ebd540b72
Trong Le <trong.p.le.92@gmail.com>
2015-12-24 00:45:15 -0500

    commit

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,100 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Le, Trong on 12/23/15.
+//  Copyright © 2015 Trong Le. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /* didSet automatically redraws when lineWidth is changed
+       Updates associated properties when value is changed */
+    var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    
+    // Set scale to not be fully on screen edge
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
+
+    // Sets color value
+    var color: UIColor = UIColor.redColor() { didSet { setNeedsDisplay() } }
+    
+    // Puts face in center
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    
+    // Sets radius of face according to bounds of screen and scale
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    private struct Scaling {
+        static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
+        static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
+        static let FaceRadiusToEyeSeparationRatio: CGFloat = 1.5
+        static let FaceRadiusToMouthWidthRatio: CGFloat = 1
+        static let FaceRadiusToMouthHeightRatio: CGFloat = 3
+        static let FaceRadiusToMouthoffsetRatio: CGFloat = 3
+    }
+    
+    // Create lines for eyes with offsets
+    private enum Eye { case Left, Right }
+    
+    private func bezierPathForEye(whichEye: Eye) -> UIBezierPath
+    {
+        let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+        
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeVerticalOffset
+        switch whichEye {
+        case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+        case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+        }
+        
+        let path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    // Create lines for mouth with offset
+    private func bezierPathForSmile(fractionOfMaxSmile: Double) -> UIBezierPath
+    {
+        let mouthWidth = faceRadius / Scaling.FaceRadiusToMouthWidthRatio
+        let mouthHeight = faceRadius / Scaling.FaceRadiusToMouthHeightRatio
+        let mouthVerticalOffset = faceRadius / Scaling.FaceRadiusToMouthoffsetRatio
+        
+        let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
+        
+        let start = CGPoint(x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthVerticalOffset)
+        let end = CGPoint(x: start.x + mouthWidth, y: start.y)
+        let cp1 = CGPoint(x: start.x + mouthWidth / 3, y: start.y + smileHeight)
+        let cp2 = CGPoint(x: end.x - mouthWidth / 3, y: cp1.y)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(start)
+        path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = lineWidth
+        
+        return path
+    }
+    
+    override func drawRect(rect: CGRect) {
+         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+        
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+        
+        let smiliness = -1.75
+        let smilePath = bezierPathForSmile(smiliness)
+        smilePath.stroke()
+    }
+
+
+}
---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Le, Trong on 12/23/15.
+//  Copyright © 2015 Trong Le. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+    
+
+}
